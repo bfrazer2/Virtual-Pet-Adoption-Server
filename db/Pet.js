4e9d4303cda25efc4d3c7ced270587263f64e83b
Benjamin Frazer <105686628+bfrazer2@users.noreply.github.com>
2023-08-10 10:36:14 -0400

    updating repo management

---
+++ db/Pet.js
@@ -0,0 +1,23 @@
+const {Sequelize, sequelize} = require('./db');
+
+const Pet = sequelize.define('pet', {
+  name: Sequelize.STRING,
+  breed: Sequelize.STRING,
+  age: Sequelize.INTEGER,
+  weight: Sequelize.INTEGER,
+  primaryColor: Sequelize.STRING,
+  secondaryColor: Sequelize.STRING,
+  hunger: Sequelize.INTEGER,
+  thirst: Sequelize.INTEGER,
+  friendship: Sequelize.INTEGER,
+  favorite: Sequelize.BOOLEAN,
+});
+
+Pet.associate = function(models) {
+  Pet.belongsTo(models.User, {
+    foreignKey: 'userId',
+    as: 'user',
+  });
+};
+
+module.exports = { Pet };
---
+++ db/seedData.js
@@ -0,0 +1,70 @@
+const pets = [
+  {
+    name: "Samson",
+    breed: "Domestic Shorthair",
+    age: 3,
+    weight: 14,
+    primaryColor: 'grey',
+    secondaryColor: 'white',
+    hunger: 100,
+    thirst: 100,
+    friendship: 50,
+    favorite: false,
+  },
+  {
+    name: "Tilly",
+    breed: "Domestic Shorthair",
+    age: 3,
+    weight: 9,
+    primaryColor: 'black',
+    secondaryColor: 'white',
+    hunger: 100,
+    thirst: 100,
+    friendship: 50,
+    favorite: false,
+  },
+  {
+    name: "Jada",
+    breed: "Boxer",
+    age: 13,
+    weight: 42,
+    primaryColor: 'brown',
+    secondaryColor: 'white',
+    hunger: 100,
+    thirst: 100,
+    friendship: 50,
+    favorite: false,
+  },
+  {
+    name: "Gabby",
+    breed: "Spaniel",
+    age: 11,
+    weight: 25,
+    primaryColor: 'gold',
+    secondaryColor: 'white',
+    hunger: 100,
+    thirst: 100,
+    friendship: 50,
+    favorite: false,
+  },
+];
+
+const users = [
+  {
+    auth0Id: "david2",
+    name: "David Petowner",
+    password: "david2pass",
+    email: "david2@example.com"
+  },
+  {
+    auth0Id: "laura1",
+    name: "Laura Petowner",
+    password: "laura1pass",
+    email: "laura1@example.com"
+  },
+]
+
+module.exports = {
+  pets,
+  users
+}
